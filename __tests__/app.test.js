2176cf0dc251d3ee80b8d5716fb4ff008591a308
Chase Crowder <gcrowder93@gmail.com>
2022-03-29 12:57:13 -0700

    signup user test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('secret-scratch routes', () => {
   beforeEach(() => {
@@ -12,11 +13,14 @@ describe('secret-scratch routes', () => {
     pool.end();
   });
 
-  it('signs up a use via POST', async () => {
+  it('signs up a user via POST', async () => {
     const res = await request(app)
-      .post('/api/v1/users/sessions')
-      .send({ username: 'chase', password: 'password' });
+      .post('/api/v1/users/register')
+      .send({ email: 'chase@gmail.com', password: 'password' });
 
-    expect(res.body).toEqual({ id: expect.any(String), username: 'chase' });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'chase@gmail.com',
+    });
   });
 });
