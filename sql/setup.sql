755f5ae8671c36cd47a510a957a6762401ca1a82
Chase Crowder <gcrowder93@gmail.com>
2022-03-29 11:51:35 -0700

    tables set up, app routes created, moving onto tests

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS secret;
+
+CREATE TABLE secret (
+    id BIGINT GENERATED AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id BIGINT GENERATED AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
